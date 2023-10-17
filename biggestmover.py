90081cb65c1748ae65a78dfa03f95c5244fadda0
lazy-geeek <lazy-geek@outlook.com>
2023-10-17 11:28:06 +0200

    Initial

---
+++ biggestmover.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import onecall
+import time
+import schedule
+
+from decouple import config
+from pprint import pprint
+
+size = 1
+symbol = 'BTCUSD'
+
+onecall = onecall.phemex(
+    key=config("PHEMEX_ID"),
+    secret=config("PHEMEX_SECRET"),
+    debug=True
+)
+
+def ask_bid(symbol=symbol):
+    # orderbook data
+
+    ob = onecall.get_orderbook(symbol,False)
+    ask = ob['result']['book']['asks'][0][0]
+    bid = ob['result']['book']['bids'][0][0]
+    pprint(bid)
+    
+    return ask, bid
+
+def bot():
+    askbid = ask_bid(symbol)
+    ask = askbid[0]
+    bid = askbid[1]
+
+    #onecall.limit_order(symbol, onecall.BUY_SIDE, size, bid)
+    #onecall.limit_order(symbol, onecall.SELL_SIDE, size, ask)
+    
+
+# identify biggest movers % up and down
+
+
+
---
+++ risk_helper.py
@@ -0,0 +1,50 @@
+import ccxt
+import math
+import pandas as pd
+import time, schedule
+import warnings
+import numpy as np
+
+from decouple import config
+from pprint import pprint
+from datetime import datetime
+
+###################### INPUTS ###################
+
+symbol = 'ETHUSD'
+size = 50000
+lockout_loss = 5000
+mo_size = size / 4
+leverage = 30
+num_orders = 2
+max_time = 4200
+max_daily_trades = 3
+
+traget = 800
+max_loss = -37
+
+tp_spread = 2
+target_multiple = 3
+
+#################################################
+
+phemex_config = {
+    'enableRateLimit': True,
+    'apiKey': config("PHEMEX_TESTNET_ID"),
+    'secret': config("PHEMEX_TESTNET_SECRET"),
+    #'test': True    
+    }
+
+phemex = ccxt.phemex(phemex_config)
+phemex.set_sandbox_mode(True)
+
+def max_daily_trades():
+    
+    max = max_daily_trades
+    closed = phemex.fetch_closed_orders(symbol)
+    closed_df = pd.DataFrame.from_dict(closed)
+    closed_df = closed_df[['filled', 'lastTradeTimestamp']]
+    pprint(closed_df)
+    
+
+max_daily_trades()
